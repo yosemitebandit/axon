5db45f2203fec38e1926c50fbda09d8c6c29dcff
Matt Ball <matt.ball.2@gmail.com>
2016-06-03 10:00:57 -0700

    adds matrix product

--- axon/util.py
+++ axon/util.py
@@ -11,6 +11,25 @@ def col_vector_sum(a, b):
   return result
 
 
+def matrix_product(a, b):
+  """Calculate the product between 2D matrices."""
+  # Dimension check: len of a's cols must equal len of b's rows.
+  if len(a[0]) != len(b):
+    raise TypeError
+  # Allocate.
+  result = [[0 for _ in b[0]] for _ in a]
+  # Compute the dot product to find the values in the result matrix.
+  for row_index in range(len(result)):
+    for col_index in range(len(result[0])):
+      a_values = a[row_index]
+      b_values = [row[col_index] for row in b]
+      dot_product = 0
+      for i, a_value in enumerate(a_values):
+        dot_product += a_value * b_values[i]
+      result[row_index][col_index] = dot_product
+  return result
+
+
 def softmax(values):
   """Compute the softmax."""
   return [math.exp(v) / sum([math.exp(v) for v in values]) for v in values]
--- test_all.py
+++ test_all.py
@@ -1,8 +1,44 @@
+import pytest
+
 import axon
 
 
 def test_col_vector_sum():
-  a = [[0], [1], [2]]
-  b = [[3], [4], [5]]
-  expected = [[3], [5], [7]]
+  a = [[0],
+       [1],
+       [2]]
+  b = [[3],
+       [4],
+       [5]]
+  expected = [[3],
+              [5],
+              [7]]
   assert axon.util.col_vector_sum(a, b) == expected
+
+
+def test_matrix_product_dim_check():
+  a = [[0],
+       [1],
+       [2]]
+  b = [[3],
+       [4]]
+  with pytest.raises(TypeError):
+    axon.util.matrix_product(a, b)
+
+
+def test_matrix_product_one_by_one():
+  a = [[3]]
+  b = [[2]]
+  expected = [[6]]
+  assert axon.util.matrix_product(a, b) == expected
+
+
+def test_matrix_product_larger():
+  a = [[0, 1, 2],
+       [2, 3, 2]]
+  b = [[2, 3, 4, 0],
+       [1, 2, 3, 0],
+       [2, 4, 6, 0]]
+  expected = [[5,  10, 15, 0],
+              [11, 20, 29, 0]]
+  assert axon.util.matrix_product(a, b) == expected
