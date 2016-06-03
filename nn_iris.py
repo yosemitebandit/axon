72542ba89d0beafed21a09112e2c00557b96ff35
Matt Ball <matt.ball.2@gmail.com>
2016-06-02 19:29:41 -0700

    start of layer-driven arch

--- nn_iris.py
+++ nn_iris.py
@@ -3,6 +3,7 @@
 
 import math
 import random
+import sys
 
 import matplotlib.pyplot as plt
 
@@ -38,6 +39,7 @@ class Node(object):
     self.parent_nodes = []
     self.child_nodes = []
     self.parent_weights = []
+    self.parent_weight_deltas = []
     self.child_weights = []
     self.bias = random.random() * 2 - 1
     self.delta = None
@@ -57,7 +59,7 @@ class Node(object):
       self.value += self.parent_weights[i] * parent_node.value
     self.value += self.bias
     # ReLU activation.
-    #self.value = max(0, self.value)
+    # self.value = max(0, self.value)
     # Sigmoid activation.
     self.value = 1 / (1 + math.exp(-self.value))
 
@@ -74,41 +76,90 @@ class Node(object):
       for i, child_node in enumerate(self.child_nodes):
         child_errors_sum += child_node.delta * self.child_weights[i]
       self.delta = self.value * (1 - self.value) * child_errors_sum
-    # Update parent weights.
+    # Get parent weight deltas.
     for i, parent_node in enumerate(self.parent_nodes):
-      weight_delta = -1 * learning_rate * self.delta * parent_node.value
-      self.parent_weights[i] += weight_delta
+      self.parent_weight_deltas[i] = (
+        -1 * learning_rate * self.delta * parent_node.value)
+    # Separately update weights.
+    for i, parent_node in enumerate(self.parent_nodes):
+      self.parent_weights[i] += self.parent_weight_deltas[i]
     # Update bias term.
     self.bias += learning_rate * self.delta
 
 
-# Setup input layer.
-input_nodes = 4
-input_layer = []
-for _ in range(input_nodes):
-  input_layer.append(Node(kind='input'))
+class Layer(object):
+  def __init__(self, kind, size):
+    self.kind = kind
+    # Want a size x 1 sized col vector.
+    self.values = [[0] for _ in range(size)]
+    self.parent_layer = None
+    self.child_layer = None
+    self.weights = []
+    self.biases = [[random.random() * 2 - 1] for _ in range(size)]
+    self.deltas = []
 
+  def __repr__(self):
+    return 'Layer (%s), %s nodes' % (self.kind, len(self.values))
+
+  def connect_to_parent(self, parent_layer):
+    self.parent_layer = parent_layer
+    # Init weights -- each inner array is a row..
+    # Parent layer defines the cols.
+    self.weights = [
+      [0 for _ in parent_layer.values] for _ in self.values]
+    # Setup child layer as well.
+    parent_layer.child_layer = self
+
+
+# Setup input layer.
+input_layer = Layer('input', 4)
+
+# Setup the first hidden layer.
+hidden_layer_one = Layer('hidden', 9)
+hidden_layer_one.connect_to_parent(input_layer)
+
+# Second the second hidden layer.
+hidden_layer_two = Layer('hidden', 5)
+hidden_layer_two.connect_to_parent(hidden_layer_one)
+
+# Setup output layer.
+output_layer = Layer('output', 3)
+output_layer.connect_to_parent(hidden_layer_two)
+
+
+print input_layer
+print hidden_layer_one
+print hidden_layer_two
+print output_layer
+sys.exit()
+
+
+# Setup input layer.
+# input_nodes = 4
+# input_layer = []
+# for _ in range(input_nodes):
+  # input_layer.append(Node(kind='input'))
 
 # Setup a single hidden layer.
-hidden_nodes = 4
-hidden_layer = []
-for _ in range(hidden_nodes):
-  hidden_node = Node(kind='hidden')
-  for input_node in input_layer:
-    weight = 2*random.random() - 1
-    hidden_node.set_parent_with_weight(input_node, weight)
-  hidden_layer.append(hidden_node)
+# hidden_nodes = 4
+# hidden_layer = []
+# for _ in range(hidden_nodes):
+  # hidden_node = Node(kind='hidden')
+  # for input_node in input_layer:
+    # weight = 2*random.random() - 1
+    # hidden_node.set_parent_with_weight(input_node, weight)
+  # hidden_layer.append(hidden_node)
 
 
 # Setup the output layer.
-output_nodes = 3
-output_layer = []
-for _ in range(output_nodes):
-  output_node = Node(kind='output')
-  for hidden_node in hidden_layer:
-    weight = 2*random.random() - 1
-    output_node.set_parent_with_weight(hidden_node, weight)
-  output_layer.append(output_node)
+# output_nodes = 3
+# output_layer = []
+# for _ in range(output_nodes):
+  # output_node = Node(kind='output')
+  # for hidden_node in hidden_layer:
+    # weight = 2*random.random() - 1
+    # output_node.set_parent_with_weight(hidden_node, weight)
+  # output_layer.append(output_node)
 
 
 def forward_propagate(data):
@@ -123,11 +174,9 @@ def forward_propagate(data):
   n2.value = data['sepal_width']
   n3.value = data['petal_length']
   n4.value = data['petal_width']
-
   # Update the hidden layer.
   for node in hidden_layer:
     node.forward_propagate()
-
   # Update output layer.
   for node in output_layer:
     node.forward_propagate()
